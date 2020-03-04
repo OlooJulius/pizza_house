f9068d0bbc8af7581f39828f05613266d77cca7b
Oloo Julius <juliusolooolero@gmail.com>
2020-03-04 12:16:23 +0300

    adding the business logic

--- js/script.js
+++ js/script.js
@@ -1,3 +1,48 @@
+// Business Logic
+var totalPriceArray = []; //Only global variable in code
+function Order (customSize, cheese) {
+  this.customSize = customSize;
+  this.sauce = 1;
+  this.cheese = cheese;
+  this.veggie1 = 1;
+  this.veggie2 = 1;
+  this.meat = 2;
+  this.pizzaPrice = 0;
+}
+Order.prototype.pizzaCost = function () {
+  if (this.customSize === "Small 10 in.") {
+    this.pizzaPrice += 6;
+  } else if (this.customSize === "Medium 14 in.") {
+    this.pizzaPrice += 9;
+  } else if (this.customSize === "Large 18 in.") {
+    this.pizzaPrice += 12;
+  }
+  if (this.cheese === "cheese") {
+    this.pizzaPrice += 1;
+  } else if (this.cheese === "light cheese") {
+    this.pizzaPrice += 0.5;
+  } else if (this.cheese === "extra cheese") {
+    this.pizzaPrice += 1.5;
+  }
+  this.pizzaPrice += this.sauce;
+  this.pizzaPrice += this.veggie1;
+  this.pizzaPrice += this.veggie2;
+  this.pizzaPrice += this.meat;
+  return this.pizzaPrice;
+}
+Order.prototype.finalCost = function () {
+  var cartTotalPrice = 0;
+  for (var arrayElement = 0; arrayElement < totalPriceArray.length; arrayElement ++) {
+    cartTotalPrice += totalPriceArray[arrayElement]; /////////////  adding array contents together
+  }
+  return cartTotalPrice;
+}
+function Address (streetAddress, city, state, zipcode) {
+  this.streetAddress = streetAddress;
+  this.city = city;
+  this.zipcode = zipcode;
+  this.deliveryAddress = (streetAddress + "  " + city + ", " + zipcode);
+}
 // init js
 $(document).ready(function () {
 	//User Interface
