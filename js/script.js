a60d50c2923f20a8d821445901e882fabe6bad56
Oloo Julius <juliusolooolero@gmail.com>
2020-03-04 12:11:47 +0300

    added the business function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,52 @@
+// introduce the Business logic
+var totalPrice = [];
+function Order(customSize, cheese) {
+	this.customSize = customSize;
+	this.sauce = 1;
+	this.cheese = cheese;
+	this.veggie1 = 1;
+	this.veggie2 = 1;
+	this.meat = 2;
+	this.pizzaPrice = 0;
+}
+//get the order and pricing
+Order.prototype.pizzaCost = function () {
+  if (this.customSize === "Small 10 in.") {
+    this.pizzaPrice += 6;
+  } else if (this.customSize === "Medium 14 in.") {
+    this.pizzaPrice += 9;
+  } else if (this.customSize === "Large 18 in.") {
+    this.pizzaPrice += 12;
+  }
+  if (this.cheese === "cheese") {
+    this.pizzaPrice += 1;
+  } else if (this.cheese === "light cheese") {
+    this.pizzaPrice += 0.5;
+  } else if (this.cheese === "extra cheese") {
+    this.pizzaPrice += 1.5;
+  }
+  this.pizzaPrice += this.sauce;
+  this.pizzaPrice += this.veggie1;
+  this.pizzaPrice += this.veggie2;
+  this.pizzaPrice += this.meat;
+  return this.pizzaPrice;
+}
+Order.prototype.sideCost = function () {
+  return this.sidePrice;
+}
+Order.prototype.finalCost = function () {
+  var cartTotalPrice = 0;
+  for (var arrayElement = 0; arrayElement < totalPriceArray.length; arrayElement ++) {
+    cartTotalPrice += totalPriceArray[arrayElement]////adding the contents of array order together
+  }
+  return cartTotalPrice;
+}
+function Address(streetAddress,city,zipcode){
+	this.streetAddress =streetAddress;
+	this.city = city;
+	this.zipcode = zipcode;
+	this.deliveryAddress = (streetAddress + "  " + city + ", "+ zipcode);
+}
 // init js
 $(document).ready(function () {
 	//User Interface
@@ -10,6 +59,7 @@ $(document).ready(function () {
 		$("#address").toggle();
 		$("#pickup-btn,#delivery-btn,#landing-tagline").hide();
 	});
+
 	$("form#address-form").submit(function (event) {
 		event.preventDefault();
 		var streetAddress = $("input#street-add").val();
@@ -21,6 +71,7 @@ $(document).ready(function () {
 		$("#landing-content").hide();
 		$("#delivery-option").text("DELIVER TO: " + newAddress.deliveryAddress);
 	});
+
 	$("form#custom-pizza").submit(function (event) {
 		event.preventDefault();
 		var customSize = $("select#size").val();
