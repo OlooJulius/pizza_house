27dc785ae51d8dd311f6952cdd0406bebafa50f4
Oloo Julius <juliusolooolero@gmail.com>
2020-03-04 12:27:32 +0300

    added User interface

--- js/script.js
+++ js/script.js
@@ -1,5 +1,52 @@
-// Business Logic
-var totalPriceArray = []; //Only global variable in code
+
+// init js
+$(document).ready(function () {
+	
+	$("#pickup-btn").click(function () {
+		$("#menu").hide();
+		$("#main").show();
+		$("#delivery-option").text("PICKUP BY CUSTOMER");
+	});
+	$("#delivery-btn").click(function () {
+		$("#address").show();
+		$("#pickup-btn,#delivery-btn,#landing-tagline").hide();
+	});
+	$("form#address-form").submit(function (event) {
+		event.preventDefault();
+		var streetAddress = $("input#street-add").val();
+		var city = $("input#city-add").val();
+		var zipcode = $("input#zip-add").val();
+		var newAddress = new Address(streetAddress, city, state, zipcode)
+		$("#order-content").show();
+		$("#landing-content").hide();
+		$("#delivery-option").text("DELIVER TO: " + newAddress.deliveryAddress);
+	});
+	$("form#custom-pizza").submit(function (event) {
+		event.preventDefault();
+		var customSize = $("select#size").val();
+		var sauce = $("select#sauce").val();
+		var cheese = $("select#cheese").val();
+		var veggie1 = $("select#veggie1").val();
+		var veggie2 = $("select#veggie2").val();
+		var meat = $("select#meat").val();
+		var pizzaDetails = (customSize + " - " + sauce + ", " + cheese + ", " + veggie1 + ", " + veggie2 + ", " + meat);
+		var newPizzaOrder = new Order(customSize, cheese);
+		newPizzaOrder.pizzaCost();
+		totalPriceArray.push(newPizzaOrder.pizzaPrice);
+		$("#pizza-details-dropdown").show();
+		$("#final-cost").text(newPizzaOrder.finalCost());
+		$("#pizza-details").append("<ul><li>" + pizzaDetails + "</li></ul>");
+		$("#size, #sauce, #cheese, #veggie1, #veggie2, #meat").val("");
+	});
+	$("#pizza-details-dropdown").click(function () {
+		$("#pizza-details").toggle();
+	});
+	$("#checkout-btn").click(function() {
+    location.reload();
+  });
+});
+	// Business Logic
+var totalPriceArray = []; // global variable
 function Order (customSize, cheese) {
   this.customSize = customSize;
   this.sauce = 1;
@@ -42,47 +89,4 @@ function Address (streetAddress, city, state, zipcode) {
   this.city = city;
   this.zipcode = zipcode;
   this.deliveryAddress = (streetAddress + "  " + city + ", " + zipcode);
-}
-// init js
-$(document).ready(function () {
-	//User Interface
-	$("#pickup-btn").click(function () {
-		$("#menu").hide();
-		$("#main").show();
-		$("#delivery-option").text("PICKUP BY CUSTOMER");
-	});
-	$("#delivery-btn").click(function () {
-		$("#address").toggle();
-		$("#pickup-btn,#delivery-btn,#landing-tagline").hide();
-	});
-	$("form#address-form").submit(function (event) {
-		event.preventDefault();
-		var streetAddress = $("input#street-add").val();
-		var city = $("input#city-add").val();
-		var state = $("select#state-select").val();
-		var zipcode = $("input#zip-add").val();
-		var newAddress = new Address(streetAddress, city, state, zipcode)
-		$("#order-content").show();
-		$("#landing-content").hide();
-		$("#delivery-option").text("DELIVER TO: " + newAddress.deliveryAddress);
-	});
-	$("form#custom-pizza").submit(function (event) {
-		event.preventDefault();
-		var customSize = $("select#size").val();
-		var sauce = $("select#sauce").val();
-		var cheese = $("select#cheese").val();
-		var veggie1 = $("select#veggie1").val();
-		var veggie2 = $("select#veggie2").val();
-		var meat = $("select#meat").val();
-		var pizzaDetails = (customSize + " - " + sauce + ", " + cheese + ", " + veggie1 + ", " + veggie2 + ", " + meat);
-		var newPizzaOrder = new Order(customSize, cheese);
-		newPizzaOrder.pizzaCost();
-		totalPriceArray.push(newPizzaOrder.pizzaPrice);
-		$("#pizza-details-dropdown").show();
-		$("#final-cost").text(newPizzaOrder.finalCost());
-		$("#pizza-details").append("<ul><li>" + pizzaDetails + "</li></ul>");
-		$("#size, #sauce, #cheese, #veggie1, #veggie2, #meat").val("");
-	});
-	$("#pizza-details-dropdown").click(function () {
-		$("#pizza-details").toggle();
-	});
+};
